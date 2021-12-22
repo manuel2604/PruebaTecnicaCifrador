a239ddd01a93015919d4adbfcad1e4333a8c95e5
Manuel Narváez <manuel260497@gmail.com>
2021-12-22 16:48:16 -0500

    se agrega cifrador proyecto

---
+++ Cifrador/Controllers/LetraCifradaController.cs
@@ -0,0 +1,29 @@
+﻿using Cifrador.Application;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cifrador.Controllers
+{
+    [ApiController]
+    [Route("api/entradaCifrador")]
+    public class LetraCifradaController : ControllerBase
+    {
+        private readonly ICifrarFraseApplicationService cifrarFraseApplicationService;
+
+        public LetraCifradaController(ICifrarFraseApplicationService cifrarFraseApplicationService)
+        {
+            this.cifrarFraseApplicationService = cifrarFraseApplicationService;
+
+        }
+
+        [HttpGet]
+        [Route("{frase})")]
+        public string Get(string frase)
+        {
+            return this.cifrarFraseApplicationService.CifrarFrase(frase);
+        }
+    }
+}
